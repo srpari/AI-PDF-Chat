d168996dd81f54e72be2107a176fbfe0ddf50508
srpari <pari.ramamoorthy@gmail.com>
2025-11-05 16:27:51 -0500

    added shadcn with dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
-import Hello from "../components/Hello";
+import { ModeToggle } from "../components/mode-toggle";
 
 export default function Home() {
-  return <Hello />
+  return (
+    <main className="relative container flex min-h-screen flex-col">
+      <div className=" p-4 flex h-14 items-center justify-between supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
+        <span className="font-bold">PDF-Chat-AI-SDK</span>
+        <ModeToggle />
+      </div>
+    </main>
+  );
 }
