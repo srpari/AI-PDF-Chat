da4238d36d74f293023b8342a2abed1f5b30c352
srpari <pari.ramamoorthy@gmail.com>
2025-11-05 16:00:06 -0500

    update tailwind

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,3 +1,3 @@
 export default function Hello(){
-    return <h1> Hello </h1>;
+    return <h1 className="text-accent hover:text-accent/80 transition-colors duration-300 text-3xl font-semibold"> Hello </h1>;
 }
